e753b6d608d1c6b44ae245e1c77395c0ca274f2b
chaitushiva <32284393+chaitushiva@users.noreply.github.com>
2018-03-26 01:13:23 -0400

    Create sshrunmultiplecommands.py

---
+++ sshrunmultiplecommands.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import paramiko
+
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect('192.168.221.235', username='nuts', password='cisco', timeout =  30)
+while True:
+      cisco_cmd = raw_input("Enter cisco router cmd:")
+      stdin, stdout, stderr = ssh.exec_command(cisco_cmd)
+      print stdout.read()
+      if  cisco_cmd == 'exit': break
+ssh.close()
